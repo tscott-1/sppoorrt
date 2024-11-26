52b0b8295c8e496c69d6c99967dc17512598fd93
Tenille <elmofreak@gmail.com>
2024-11-26 19:27:54 +0800

    add navigate post login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import postLogin from "../api/post-login.js";
 
 function LoginForm() {
+    const navigate = useNavigate(); 
+
     const [credentials, setCredentials] = useState({
           username: "",
           password: "",
@@ -12,6 +17,19 @@ function LoginForm() {
               ...prevCredentials,
               [id]: value,
           }));
+    };
+
+    const handleSubmit = (event) => {
+            event.preventDefault();
+            if (credentials.username && credentials.password) {
+                postLogin(
+                    credentials.username,
+                    credentials.password
+                ).then((response) => {
+                    window.localStorage.setItem("token", response.token);
+                    navigate("/");
+            });
+            }
     };
 
     return (
@@ -34,7 +52,9 @@ function LoginForm() {
                  onChange={handleChange}
             />
         </div>
-        <button type="submit">Login</button>
+            <button type="submit" onClick={handleSubmit}>
+                Login
+            </button>
       </form>
     );
   }
