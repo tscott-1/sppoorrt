0a9b98d7d721a7791c9e15f2a4f451cf20cb8974
Tenille <elmofreak@gmail.com>
2024-11-27 06:40:42 +0800

    fix issue with user signup and add some css

--- src/api/post-signup.js
+++ src/api/post-signup.js
@@ -1,4 +1,4 @@
-async function postSignup(username, firstname, email, password) {
+async function postSignup(username, first_name, email, password) {
     const url = `${import.meta.env.VITE_API_URL}/users/`;
     const response = await fetch(url, {
       method: "POST", // We need to tell the server that we are sending JSON data so we set the Content-Type header to application/json
