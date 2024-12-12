2849c659802f3b124a8990f14efc71ed1d7a433f
Tenille <elmofreak@gmail.com>
2024-12-12 17:09:57 +0800

    tidy loginform

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -54,10 +54,6 @@ function LoginForm() {
     }
   };
 
-  const handleSignup = (event) => {
-    event.preventDefault(); // Prevent unintended behavior
-    navigate("/signup");
-  };
 
   return (
     <form>
